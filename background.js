a3f1910fad1f3f0892b85f3de4ae1117965b3d63
gauravpandeypes <42074265+gauravpandeypes@users.noreply.github.com>
2021-05-16 13:37:15 +0530

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-console.log("background running");
-
-chrome.browserAction.onClicked.addListener(buttonClicked);
-
-function buttonClicked(tab) {
-    console.log("button clicked");
-    
-    chrome.tabs.sendMessage(tab.id, "run");
-}
